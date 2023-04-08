aca5e2aa5fec1395f2740a43ec7193d119b1fc06
Judith <judith.caldes@estudiantat.upc.edu>
2023-04-08 11:20:10 +0200

    arreglado alfa1

--- src/vad_docopt.h
+++ src/vad_docopt.h
@@ -309,7 +309,7 @@ int elems_to_args(Elements *elements, DocoptArgs *args, bool help,
 
 DocoptArgs docopt(int argc, char *argv[], bool help, const char *version) {
     DocoptArgs args = {
-        0, 0, 0, (char*) "10", NULL, NULL, NULL,
+        0, 0, 0, (char*) "1", (char*) "10", NULL, NULL, NULL,
         usage_pattern, help_message
     };
     Tokens ts;
